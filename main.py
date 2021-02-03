39d621ad602a57259b484d0685b5edd04d35ae6e
prollyhollywood <72751673+prollyhollywood@users.noreply.github.com>
2021-02-03 00:47:16 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 while True:
     print (input.acceleration(Dimension.Z))
-    if input.acceleration(Dimension.Z) > 10:
+    if input.acceleration(Dimension.Z) > 1000:
         light.set_all(light.rgb(255,255,0))
-    elif input.acceleration(Dimension.Z) > 6:
+    elif input.acceleration(Dimension.Z) > 600:
         light.set_all(light.rgb(255,0,255))
-    elif input.acceleration(Dimension.Z) > 3:
-        light.set_all(light.rgb(255,0,255))
+    elif input.acceleration(Dimension.Z) > 300:
+        light.set_all(light.rgb(0,255,255))
     else:
         light.clear()
--- main.ts
+++ main.ts
@@ -1,11 +1,11 @@
 while (true) {
     console.log(input.acceleration(Dimension.Z))
-    if (input.acceleration(Dimension.Z) > 10) {
+    if (input.acceleration(Dimension.Z) > 1000) {
         light.setAll(light.rgb(255, 255, 0))
-    } else if (input.acceleration(Dimension.Z) > 6) {
+    } else if (input.acceleration(Dimension.Z) > 600) {
         light.setAll(light.rgb(255, 0, 255))
-    } else if (input.acceleration(Dimension.Z) > 3) {
-        light.setAll(light.rgb(255, 0, 255))
+    } else if (input.acceleration(Dimension.Z) > 300) {
+        light.setAll(light.rgb(0, 255, 255))
     } else {
         light.clear()
     }
